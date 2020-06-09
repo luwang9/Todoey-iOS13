154cf8ace59eadf12c6351721cf2b1b464227ccf
luwang9 <51976455+luwang9@users.noreply.github.com>
2020-06-09 10:48:55 -0400

    use UserDfaults to store small bits of data

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
+    
     var itemArray = ["Find egg","Find mom","Go to bathroom"]
+    let defaults = UserDefaults.standard
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        if let array = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = array
+        }
     }
 
     //MARK: - Tableview Datasource Methods
@@ -41,6 +46,7 @@ class TodoListViewController: UITableViewController {
         let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             self.itemArray.append(textField.text!)
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
             self.tableView.reloadData()
             
         }
