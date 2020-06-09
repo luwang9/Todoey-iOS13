ca60c5aa8ec9267ac2ad9d2cfda67586b774364b
luwang9 <51976455+luwang9@users.noreply.github.com>
2020-06-09 14:39:19 -0400

    use class to store cell information

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -10,12 +10,17 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    var itemArray = ["Find egg","Find mom","Go to bathroom"]
+    var itemArray = [Item]()
     let defaults = UserDefaults.standard
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        if let array = defaults.array(forKey: "TodoListArray") as? [String] {
+         //Do any additional setup after loading the view.
+        
+        let newitem = Item()
+        newitem.name = "Cooking"
+        itemArray.append(newitem)
+        
+        if let array = defaults.array(forKey: "TodoListArray") as? [Item] {
             itemArray = array
         }
     }
@@ -27,16 +32,16 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let toDoCur = itemArray[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
-        cell.textLabel?.text = toDoCur
+        cell.textLabel?.text = toDoCur.name
+        let item = itemArray[indexPath.row]
+        cell.accessoryType = item.done ? UITableViewCell.AccessoryType.checkmark : .none
+        
         return cell
     }
     //MARK: - Tableview Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
     }
     //MARK: - Add new items
@@ -45,7 +50,9 @@ class TodoListViewController: UITableViewController {
         var textField = UITextField()
         let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
-            self.itemArray.append(textField.text!)
+            let newitem = Item()
+            newitem.name = textField.text!
+            self.itemArray.append(newitem)
             self.defaults.set(self.itemArray, forKey: "TodoListArray")
             self.tableView.reloadData()
             
---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Lu Wang on 6/9/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var name : String = ""
+    var done : Bool = false
+}
